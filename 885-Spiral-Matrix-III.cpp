e34f987ec1b9538b5932de6a7a00c6c17fc20dab
Sithaarth M <sithumahesh24@gmail.com>
2024-08-08 14:00:37 +0530

    [885] [Time Beats: 86.90%] [Memory Beats: 89.42%] - LeetPush

--- 885-Spiral-Matrix-III.cpp
+++ 885-Spiral-Matrix-III.cpp
@@ -3,33 +3,52 @@ public:
     vector<vector<int>> spiralMatrixIII(int rows, int cols, int rstrt,
                                         int cstrt) {
         int c = 1;
-        vector<vector<int>> res;
-        while(c < 2*rows || c < 2* cols) {
+        vector<vector<int>> res(rows*cols,vector<int>(2,{}));
+        int i,k = 0;
+        int m = max(2*rows,2*cols);
+        while(c < m) {
             if (c % 2) {
                 if(rstrt >=0 && rstrt< rows){
-                    for(int i = cstrt;i<cstrt+c;i++)
-                        if(i >=0 && i<cols)
-                            res.push_back({rstrt,i});
+                    i = (cstrt < 0)?0 : cstrt;
+                    for(i;i<cstrt+c;i++)
+                    {
+                        if( i < cols) res[k++] = {rstrt,i};
+                        else break;
+                    }
                 }
                 cstrt += c;
                 if(cstrt >=0 && cstrt< cols){
-                    for(int i = rstrt;i<rstrt+c;i++)
-                        if(i >=0 && i<rows)
-                            res.push_back({i,cstrt});
+                    i = (rstrt < 0)?0 : rstrt;
+                    for(i;i<rstrt+c;i++)
+                    {
+                        if( i < rows) res[k++] = {i,cstrt};
+                        else break;
+                    }
                 }
                 rstrt += c;
 
-            } else {
+            } 
+            else {
                 if(rstrt >=0 && rstrt< rows){
-                    for(int i = cstrt;i>cstrt-c;i--)
-                        if(i >=0 && i<cols)
-                            res.push_back({rstrt,i});
+                    i = (cstrt >= cols)?cols-1:cstrt;
+                    for(i;i>cstrt-c;i--)
+                    {
+                        if(i < cols){
+                            if( i >= 0) res[k++] = {rstrt,i};
+                            else break;
+                        }
+                    }
                 }
                 cstrt -= c;
                 if(cstrt >=0 && cstrt< cols){
-                    for(int i = rstrt;i>rstrt-c;i--)
-                        if(i >=0 && i<rows)
-                            res.push_back({i,cstrt});
+                    i = (rstrt >= rows)?rows-1:rstrt;
+                    for(i;i>rstrt-c;i--)
+                    {
+                        if(i < rows){
+                            if( i >= 0) res[k++] = {i,cstrt};
+                            else break;
+                        }
+                    }
                 }
                 rstrt -= c;
             }
