ea077d668fb10e677e7d8b6a5d5d7a957780d76e
Sithaarth M <sithumahesh24@gmail.com>
2024-08-12 10:39:04 +0530

    [703] [Time Beats: 86.49%] [Memory Beats: 39.10%] - LeetPush

--- 703-Kth-Largest-Element-in-a-Stream.cpp
+++ 703-Kth-Largest-Element-in-a-Stream.cpp
@@ -2,11 +2,8 @@ class KthLargest {
     int k;
     priority_queue<int,vector<int>,greater<int>> pq;
 public:
-    KthLargest(int k1, vector<int>& nums1) {
-        k = k1;
-        for(auto& i : nums1)
-            pq.push(i);
-        while(!pq.empty() && pq.size() > k)
+    KthLargest(int k1, vector<int>& nums1):pq(nums1.begin(),nums1.end()),k(k1) {
+        while(pq.size() > k)
             pq.pop();
     }
     
